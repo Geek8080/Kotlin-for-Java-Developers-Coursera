5bdfe3440a82bc2729935e23e866dc094a994eb0
Prashant Prakash <prashantprakash97@gmail.com>
2020-10-11 18:50:13 +0530

    Added Slides and Playground solutions for week 3

---
+++ Week 3/1- Nullability/SafeCasts.kt
@@ -0,0 +1,20 @@
+/**
+ * Type cast as throws ClassCastException, if
+ * the cast is unsuccessful. Safe cast as? returns
+ * null, if the cast is unsuccessful. Declare the
+ * s variable to make the first line print null and
+ * the second one throw an exception.
+ *
+ * fun main(args: Array<String>) {
+ *     val s = TODO()
+ *     println(s as? Int) // null
+ *     println(s as Int?) // exception
+ * }
+ *
+ */
+
+ fun main(args: Array<String>) {
+    val s = "hello"
+    println(s as? Int) // null
+    println(s as Int?) // exception
+}
