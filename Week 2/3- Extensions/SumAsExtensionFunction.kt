a649b6ebd38dd9387bb6b40a6034af30d662367c
Prashant Prakash <prashantprakash97@gmail.com>
2020-10-11 18:47:16 +0530

    Added Slides and Playground solutions for Week 2

---
+++ Week 2/3- Extensions/SumAsExtensionFunction.kt
@@ -0,0 +1,32 @@
+/**
+ * Change the 'sum' function so that it was declared as an extension to List<Int>.
+ */
+
+/**
+ * fun sum(list: List<Int>): Int {
+ *     var result = 0
+ *     for (i in list) {
+ *         result += i
+ *     }
+ *     return result
+ * }
+ *
+ * fun main(args: Array<String>) {
+ *     val sum = sum(listOf(1, 2, 3))
+ *     println(sum) // 6
+ * }
+ *
+ */
+
+ fun List<Int>.sum(): Int {
+    var result = 0
+    for (i in this) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = (listOf(1, 2, 3))
+    println(sum.sum()) // 6
+}
